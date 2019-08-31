ca52773e87d163dfd987aef336c4b82b8ea9f7cb
richard@electronicsdesign.nl <richard@electronicsdesign.nl>
2019-08-31 21:17:48 +0200

    Added mock code to test without an actual raspi. Next feature: implement function to return the difference between two stepper configurations so we can efficiently update the interface

--- code/mock_stepper_control.py
+++ code/mock_stepper_control.py
@@ -1,8 +1,6 @@
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 import time
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
 
 motor_control_pins = [
     ([11,  7, 15, 13], "Alice"),
@@ -11,46 +9,40 @@ motor_control_pins = [
     ([36, 37, 38, 40], "Derek"),
     ([23, 21, 19, 10], "Eddie")]
 
-for motor in motor_control_pins:
-    for pin in motor:
-        GPIO.setup(pin, GPIO.OUT)
-        GPIO.output(pin, 0)
-  
 halfstep_seq = [
-    [1,0,0,0],
-    [1,1,0,0],
-    [0,1,0,0],
-    [0,1,1,0],
-    [0,0,1,0],
-    [0,0,1,1],
-    [0,0,0,1],
-    [1,0,0,1]]
+    [1, 0, 0, 0],
+    [1, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 1, 0],
+    [0, 0, 1, 0],
+    [0, 0, 1, 1],
+    [0, 0, 0, 1],
+    [1, 0, 0, 1]]
 
 
 def reverse_motor(motor):
     return ([motor[0][2], motor[0][1], motor[0][0], motor[0][3]], motor[1])
+#end def
+
 
 def set_5_indents(levels):
-    steps_per_level = 512
+    steps_per_level = 1
     motor_steps = [0] * 5
 
     for i in range(5):
         motor_steps[i] = levels[i] * steps_per_level
-        
-    for current_full_step in range(max(levels) * steps_per_level):                
-        for halfstep in range(8):
-            i = 0
-            for motor in motor_control_pins:
-                print(i)
-                for pin in range(4):
-                    if (current_full_step < motor_steps[i]):
-                        if (levels[i] < 0):
-                            GPIO.output(reverse_motor(motor)[0][pin], halfstep_seq[halfstep][pin])
-                        elif (levels[i] > 0): 
-                            GPIO.output(motor[0][pin], halfstep_seq[halfstep][pin])
-                i = i + 1
-            
-            time.sleep(0.001)
+
+    for current_full_step in range(max(levels) * steps_per_level):
+        i = 0  # Keeps track of the motor indices
+        for motor in motor_control_pins:
+            if (current_full_step < abs(motor_steps[i])):
+                if (levels[i] > 0):
+                    print("Forward: {0}", motor[1])
+                elif (levels[i] < 0):
+                    print("Reverse: {0}", motor[1])
+                else:
+                    pass  # The else-clause should never be visited
+            i = i + 1
 #end def
 
 
@@ -76,12 +68,6 @@ def get_5_indent_values(in_buffer, start_line):
     return in_buffer[start_line:start_line+5]
 #end def
 
-
-#############################################################
-# Start of implementation                                   #
-#############################################################
-
-
 indents = load_indents_from_file("mock_stepper_control.py")
 
 # Display all indents from the file
@@ -92,5 +78,3 @@ for e in indents:
 five_indents = get_5_indent_values(indents, 37)
 # TODO: calculate relative indentations 
 set_5_indents(five_indents)
-
-GPIO.cleanup()

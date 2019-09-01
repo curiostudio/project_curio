ffa4aef0c34ae3dd81f53906f2e0eeb2fd056786
richard@electronicsdesign.nl <richard@electronicsdesign.nl>
2019-09-01 21:14:09 +0200

    Added user interface for testing

--- code/mock_stepper_control.py
+++ code/mock_stepper_control.py
@@ -20,8 +20,23 @@ halfstep_seq = [
     [1, 0, 0, 1]]
 
 
+def static_vars(**kwargs):
+    """ 
+    Custom decorator to allow declaration of static variables like this:\n
+    @static_var(variable = 0)\n
+    def foo(parameters):\n
+        foo.variable += 1
+    """
+    def decorate(func):
+        for k in kwargs:
+            setattr(func, k, kwargs[k])
+        return func
+    return decorate
+# end def
+
+
 def reverse_motor(motor):
-    return ([motor[0][2], motor[0][1], motor[0][0], motor[0][3]], motor[1])
+    return [motor[2], motor[1], motor[0], motor[3]]
 # end def
 
 
@@ -35,10 +50,10 @@ def set_5_indents(levels):
     for current_full_step in range(max(levels) * steps_per_level):
         i = 0  # Keeps track of the motor indices
         for motor in motor_control_pins:
-            if (current_full_step < abs(motor_steps[i])):
-                if (levels[i] > 0):
+            if current_full_step < abs(motor_steps[i]):
+                if levels[i] > 0:
                     print("Forward: {0}", motor[1])
-                elif (levels[i] < 0):
+                elif levels[i] < 0:
                     print("Reverse: {0}", motor[1])
                 else:
                     pass  # The else-clause should never be visited
@@ -47,7 +62,7 @@ def set_5_indents(levels):
 
 
 def load_indents_from_file(filename):
-    " load all indent information from the file and returns a list of indentations "
+    " Load all indent information from the file and returns a list of indentations "
     filehandle = open(filename, 'r')
     ind = []
 
@@ -65,18 +80,59 @@ def load_indents_from_file(filename):
 
 
 def get_5_indent_values(in_buffer, start_line):
-    " returns five lines from the input buffer "
+    " Returns five lines from the input buffer "
     return in_buffer[start_line:start_line+5]
+# end def
+
+
+def get_5_indent_deltas(old, new):
+    return [(new[i] - old[i]) for i in range(5) if (i < len(new)) and (i < len(old))]
+# end def
+
+@static_vars(page = [0] * 5)
+@static_vars(previous_page = [0] * 5)
+def update_interface(start_line):
+    " Calculates the new state of the interface "
+    update_interface.page = get_5_indent_values(indents, active_line)
+    update_interface.previous_page = update_interface.page
 # end def
 
 
 indents = load_indents_from_file("mock_stepper_control.py")
 
-# Display all indents from the file
-for e in indents:
-    print(e)
+active_line = 0
+display_lines = 5
+total_lines = len(indents)
+userinput = ""
 
-# Select five indent values from a certain linenumber
-five_indents = get_5_indent_values(indents, 37)
-# TODO: calculate relative indentations
-set_5_indents(five_indents)
+if total_lines <= 5:
+    print ("Showing lines: 1 - %d" % (total_lines))
+    # Set steppers en do nothing
+    pass
+else:
+    while True:
+
+        # User selected Next page
+        if (userinput.upper() == "N") and ((active_line + 5) <= total_lines): 
+            active_line = active_line + 5
+            display_lines = 5
+
+        # User selected Previous page
+        elif (userinput.upper() == "P") and ((active_line - 5) >= 0):
+            active_line = active_line - 5
+            display_lines = 5
+
+        # User selected Quit program
+        elif userinput.upper() == "Q":
+            break
+
+        # User entered invalid character
+        else:
+            pass
+
+        if active_line + display_lines > total_lines:
+            display_lines = total_lines - active_line
+
+        print ("Showing lines: %d - %d" % (active_line + 1, active_line + display_lines))
+        userinput = input("(N)ext | (P)revious | (Q)uit > ")
+    # end while
